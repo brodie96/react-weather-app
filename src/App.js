0d6dc59d69483bbb6bd9d271bcb49d275ddaffb1
brodie96 <101592243+brodie96@users.noreply.github.com>
2022-10-18 18:02:52 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Coded by{" "}
-        <a href="https://leafy-begonia-db17ce.netlify.app" target="_blank">
-          Rebecca Brodie
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/brodie96/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://leafy-begonia-db17ce.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rebecca Brodie
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/brodie96/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

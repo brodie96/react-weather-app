ef669c76c5e30c42307993d316d3e4d0bb636e81
brodie96 <101592243+brodie96@users.noreply.github.com>
2022-10-18 17:43:29 +0200

    added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Coded by{" "}
+        <a href="https://leafy-begonia-db17ce.netlify.app" target="_blank">
+          Rebecca Brodie
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/brodie96/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }

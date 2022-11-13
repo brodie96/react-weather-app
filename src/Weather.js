84bee6c10632a4c6bffe0628368e2e5fb665919f
brodie96 <101592243+brodie96@users.noreply.github.com>
2022-11-13 20:12:39 +0100

    added day and time

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
+
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -14,7 +16,7 @@ export default function Weather(props) {
       description: response.data.condition.description,
       humidity: response.data.temperature.humidity,
       iconUrl: `"http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png"`,
-      date: "Tuesday 09:00",
+      date: new Date(response.data.time * 1000),
     });
   }
   if (weatherData.ready) {
@@ -41,7 +43,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
